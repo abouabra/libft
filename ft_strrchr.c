badab923f56a9db6a6ee582e93139f713ea5e50c
abouabra <aymanbouabra@gmail.com>
2022-10-05 16:20:55 +0100

    1337

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: abouabra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 13:26:21 by abouabra          #+#    #+#             */
-/*   Updated: 2022/10/05 16:19:04 by abouabra         ###   ########.fr       */
+/*   Updated: 2022/10/05 16:20:48 by abouabra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	const char	*s;
 	int i;
 
 	i = ft_strlen(str);
